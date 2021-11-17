689d2eb2fc6da57a6c65a8cd234b369104b3e3a4
raafacoelho <ra_faellacoelho@hotmail.com>
2021-11-17 16:26:33 -0300

    Creating the structure of components.

---
+++ src/components/Article/index.js
@@ -0,0 +1,12 @@
+import Post from '../Post';
+
+const Article = () => {
+    return (
+        <>
+            <h1>Opa Opa</h1>
+            <Post />
+        </>
+    );
+}
+
+export default Article;
