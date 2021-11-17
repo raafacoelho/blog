154d6677ca41964638401b7b9d0b3961caa31c26
raafacoelho <ra_faellacoelho@hotmail.com>
2021-11-17 17:13:15 -0300

    Styling the menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,30 @@
+import logosvg from '../../svg/logo.svg'
 
 const Header = () => {
+
+    const descLogo = "Essa é a logo do Blog"
+
     return (
         <>
-            <h1>Opa</h1>
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logosvg} alt={descLogo} />
+                </div>
+                <div className="search">
+                    <input type="text" className="input-search" name="search" placeholder="Buscar artigos, podcasts, destaques" />
+                </div>
+                <ul className="menu">
+                    <li>
+                        <a href="#" className="nav-link">Categories</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link">About</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </header>
         </>
     );
 }
